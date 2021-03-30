3ec3052199e650f4b6840b4c5cf9d1c50fa10dfb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-30 13:26:07 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <h1 class="text-3xl font-black text-[#33FF36]">
+      contax
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
