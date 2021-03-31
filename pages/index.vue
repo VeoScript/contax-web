2b687c204fe6baff37fc932c6f63ca4f1705cde7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-31 11:19:47 +0800

    style: redesign login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="h-screen flex items-center justify-center">
+  <div class="h-screen w-full flex items-center justify-center">
     <div class="flex justify-center w-full">
-      <div class="w-1/4">
-        <form @submit.prevent="">
-          <h1 class="font-black text-center text-4xl text-[#76FF03]">contax</h1>
-          <div class="font-light text-center text-sm text-[#333]">secure your phonebook and personal information</div>
-          <div class="mt-5 space-y-2">
+      <div>
+        <form @submit.prevent="login">
+          <h1 class="font-black text-center text-5xl text-[#76FF03]">contax</h1>
+          <div class="font-light mt-2 text-center text-md text-[#333]">secure your phonebook and personal information</div>
+          <div class="mt-5 space-y-1">
             <div class="space-y-1">
               <span class="text-xs ml-2">Username</span>
               <input type="text" class="w-full px-5 py-3 bg-[#CCFF90] rounded-md focus:ring-2 focus:ring-[#76FF03] focus:outline-none" />
@@ -15,8 +15,11 @@
               <input type="password" class="w-full px-5 py-3 bg-[#CCFF90] rounded-md focus:ring-2 focus:ring-[#76FF03] focus:outline-none" />
             </div>
             <div class="flex items-center justify-between space-x-2">
-              <nuxt-link class="w-full px-5 py-3 rounded-md text-center bg-[#333] focus:ring-2 focus:ring-[#35362E] text-white text-sm" to="/">Create Account</nuxt-link>
-              <button class="w-full my-2 px-5 py-3 bg-[#64DD17] text-white text-sm rounded-md focus:ring-2 focus:ring-[#76FF03] focus:outline-none">Login</button>
+              <nuxt-link class="w-full px-5 py-2 rounded-md text-center bg-[#333] focus:ring-2 focus:ring-[#35362E] text-white text-xs" to="/">Create Account</nuxt-link>
+              <button class="w-full my-2 px-5 py-2 bg-[#64DD17] text-white text-xs rounded-md focus:ring-2 focus:ring-[#76FF03] focus:outline-none">Login</button>
+            </div>
+            <div class="flex justify-center">
+              <nuxt-link class="font-light text-xs hover:underline" to="/">Forgot Password</nuxt-link>
             </div>
           </div>
         </form>
@@ -26,5 +29,11 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    login() {
+      this.$router.push("home")
+    }
+  },
+}
 </script>
