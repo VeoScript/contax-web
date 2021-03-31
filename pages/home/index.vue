8977e28eb48b804d128f1691bef6d9ff2ecf71af
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-31 14:06:04 +0800

    fix: import homecontent & footbar components

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="h-screen flex justify-center">
-    <div class="flex justify-center">
-      <div class="flex mt-10">
-        <div class="profile w-1/2">
-          <img class="w-32 h-32 rounded-full border-4 border-[#76FF03]" src="http://pm1.narvii.com/6818/069881a78d1dfde990bf0bd3637d0122cd5f4347v2_00.jpg" alt="">
-        </div>
-        <div class="info space-y-1 mt-2">
+  <div class="h-screen overflow-hidden">
+    <div class="flex items-center justify-center mt-5">
+      <div class="img flex items-center space-x-5">
+        <img class="w-32 h-32 rounded-full border-4 border-[#76FF03]" src="http://pm1.narvii.com/6818/069881a78d1dfde990bf0bd3637d0122cd5f4347v2_00.jpg" alt="profile">
+        <div class="space-y-1">
           <div class="font-black text-[#333] text-xl uppercase">Lisa Manoban</div>
           <div class="font-medium text-[#333] text-sm">lisamanoban@gmail.com</div>
           <div class="font-light text-[#333] text-xs w-2/3">
@@ -13,16 +11,23 @@
           </div>
         </div>
       </div>
-      <div class="flex space-x-5 mt-20 w-1/2">
+      <div class="rate flex space-x-3">
         <div class="font-extrabold text-lg">73 <span class="font-normal text-xs text-[#76FF03] px-3 py-0.5 rounded-full bg-[#333]">Contacts</span></div>
         <div class="font-extrabold text-lg">55 <span class="font-normal text-xs text-[#76FF03] px-3 py-0.5 rounded-full bg-[#333]">Private Files</span></div>
       </div>
     </div>
+    <HomeContent />
+    <FootBar />
   </div>
 </template>
 
 <script>
+import HomeContent from '~/components/layouts/HomeContent'
+import FootBar from '~/components/layouts/FootBar'
 export default {
-
+  components: {
+    HomeContent,
+    FootBar
+  },
 }
 </script>
