8db3a43b6a38bff9ae24c10cda3d37e9038f6b58
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-31 14:03:34 +0800

    feat: add footer component

---
+++ components/layouts/FootBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="flex justify-center mt-10">
+    <div class="font-light text-xs text-[#333]">&copy; {{ new Date().getFullYear() }}, Contax - Veoscript.</div>
+  </footer>
+</template>
