3cf5e88e6083ab609543e4180e39a62a23c520c4
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-01 12:48:01 +0800

    feat: create gallery page

---
+++ pages/gallery/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-screen overflow-hidden">
+    <HeadBar />
+    <div class="flex w-full space-x-16 justify-center mt-10">
+      <SideBar />
+      <Gallery />
+    </div>
+    <FootBar />
+  </div>
+</template>
+
+<script>
+import HeadBar from '~/components/layouts/HeadBar'
+import Gallery from '~/components/layouts/Gallery'
+import FootBar from '~/components/layouts/FootBar'
+import SideBar from '~/components/layouts/SideBar'
+export default {
+  components: {
+    HeadBar,
+    Gallery,
+    FootBar,
+    SideBar
+  },
+}
+</script>
