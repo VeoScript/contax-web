07a14ede683859433d4123ff38b22e3740ac7c03
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-04-01 12:49:20 +0800

    style: update headbar design

--- components/layouts/HeadBar.vue
+++ components/layouts/HeadBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center mt-5">
     <div class="img flex items-center space-x-5">
-      <img class="w-32 h-32 rounded-full border-4 border-[#BBF135]" src="http://pm1.narvii.com/6818/069881a78d1dfde990bf0bd3637d0122cd5f4347v2_00.jpg" alt="profile">
+      <img class="w-32 h-32 rounded-full border-4 border-[#91E53A]" src="http://pm1.narvii.com/6818/069881a78d1dfde990bf0bd3637d0122cd5f4347v2_00.jpg" alt="profile">
       <div class="space-y-1">
         <div class="font-black text-[#333] text-xl uppercase">Lisa Manoban</div>
         <div class="font-medium text-[#333] text-sm">lisamanoban@gmail.com</div>
@@ -20,8 +20,8 @@
         </nuxt-link>
       </div>
       <div class="flex space-x-3 mt-3">
-        <div class="font-extrabold text-lg">73 <span class="font-normal text-xs text-[#76FF03] px-3 py-0.5 rounded-full bg-[#333]">Contacts</span></div>
-        <div class="font-extrabold text-lg">55 <span class="font-normal text-xs text-[#76FF03] px-3 py-0.5 rounded-full bg-[#333]">Private Files</span></div>
+        <div class="font-extrabold text-lg">32 <span class="font-normal text-xs text-[#76FF03] px-3 py-0.5 rounded-full bg-[#333]">Contacts</span></div>
+        <div class="font-extrabold text-lg">10 <span class="font-normal text-xs text-[#76FF03] px-3 py-0.5 rounded-full bg-[#333]">Photos</span></div>
       </div>
     </div> 
   </div>
